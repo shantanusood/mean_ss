3a3408151cbe598da06bddddccdd20a04b98d024
Shantanu Sood <shantanusood.1989@gmail.com>
2021-05-10 19:58:46 -0400

    Updated host for value web service

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
    //public static readonly baseUrl='http://localhost:5000/';
    public static readonly baseUrl = "https://shantanusood.pythonanywhere.com/";
-   public static readonly baseUrlAnalytics='http://localhost:5000/';
+   public static readonly baseUrlAnalytics='http://192.168.1.162:5000/';
 
 }
